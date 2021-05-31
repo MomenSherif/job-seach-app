e6bacc3e6d9e8556e2ee8a869e40c8f80041902a
Momen Sherif <momensherif.2019@gmail.com>
2021-05-31 23:31:50 +0200

    Hotfix: jobs url infocard

--- src/components/organisms/RelatedJobs/RelatedJobs.tsx
+++ src/components/organisms/RelatedJobs/RelatedJobs.tsx
@@ -7,7 +7,7 @@ import { repeatElement } from '@utils';
 import { getSkillRelatedJobs } from '@api/skills';
 import styles from './RelatedJobs.module.scss';
 
-const RelatedJobs: React.FC<{ skillID: string }> = ({ skillID }) => {
+const RelatedJobs: React.FC<{ skillID: string; }> = ({ skillID }) => {
   const { data, loading, error, refetch } = useQuery(
     () => getSkillRelatedJobs(skillID),
     { enable: true },
@@ -23,17 +23,17 @@ const RelatedJobs: React.FC<{ skillID: string }> = ({ skillID }) => {
         {loading
           ? repeatElement(5, <CardSkeleton height={170} />)
           : !error &&
-            data?.map((job) => (
-              <InfoCard
-                key={job.job_uuid}
-                to={`/skills/${job.job_uuid}`}
-                title={job.job_title}
-                tags={[
-                  { label: 'Importance', value: job.importance },
-                  { label: 'Level', value: job.level },
-                ]}
-              />
-            ))}
+          data?.map((job) => (
+            <InfoCard
+              key={job.job_uuid}
+              to={`/jobs/${job.job_uuid}`}
+              title={job.job_title}
+              tags={[
+                { label: 'Importance', value: job.importance },
+                { label: 'Level', value: job.level },
+              ]}
+            />
+          ))}
         {error && <ErrorFallback error={error} onRetry={refetch} />}
       </div>
     </section>
